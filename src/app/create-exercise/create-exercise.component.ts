edbb56974e292150390350860c964f8476055a80
AndreaLaenens <andrealaenens@hotmail.com>
2018-10-04 11:37:29 +0200

    0410T1130

--- src/app/create-exercise/create-exercise.component.ts
+++ src/app/create-exercise/create-exercise.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ExerciseService } from '../../Service/ExerciseService';
 import { ExerciseDTO } from '../../model/ExerciseDTO';
 import { SpecificationDTO } from '../../model/SpecificationDTO';
-import { SpecificationService } from '../../Service/SpecificationService';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { RoutineService } from '../../Service/RoutineService';
 
 
@@ -31,6 +30,7 @@ export class CreateExerciseComponent implements OnInit {
   // keep track of selected exercise
   selectedExercise: ExerciseDTO;
   exercises: ExerciseDTO[];
+
   //specifications: SpecificationDTO[];
 
   // display
@@ -39,11 +39,11 @@ export class CreateExerciseComponent implements OnInit {
   
   
   constructor(private exerciseService: ExerciseService, 
-    private specificationService: SpecificationService, private routineService: RoutineService) { }
+    private routineService: RoutineService) { }
 
 
   ngOnInit() {
-   //this.loadExercises();
+   this.loadExercises();
   }
 
   changeStatus(){
@@ -54,80 +54,44 @@ export class CreateExerciseComponent implements OnInit {
     }
   }
 
-  /*
   loadExercises() {
     this.exerciseService.findAllExercises().subscribe(data => {
       this.exercises = data;
       });
   }
   
-  reUseExercise() {
-    //alert('reuse exercise');
-    this.saveSpecification(this.selectedExercise);
-  }
-
   newExercise() {
     let dto = new ExerciseDTO();
     dto.name = this.name;
     dto.description = this.description;
     this.exerciseService.save(dto).subscribe(data => {
-    dto = data;
-    alert(dto.id);
-    this.saveSpecification(dto);
+      dto = data;
+      this.addSpecification(dto);
+      }); 
+  }
+
+  checkExercise() {
+    if (this.name != '') {
+      this.newExercise();
+    } else if (this.selectedExercise != null) {
+      this.addSpecification(this.selectedExercise);
+    } 
+  }
+
+  addSpecification(exercise: ExerciseDTO) {
+    console.log(exercise.name+'in addSpecification');
+    let dto: SpecificationDTO = new SpecificationDTO();
+    dto.exercise = exercise;
+    dto.repetitions = this.repetitions;
+    dto.secBreak = this.secBreak;
+    dto.other = this.other;
+    let id = this.routineService.selectedRoutine.id;
+    this.routineService.addSpecification(id, dto).subscribe(data =>{
+      this.changed.emit();
+      this.changeStatus();
+      let dto: SpecificationDTO = data;
+      alert('this is the exercise'+ exercise.name)
     });
   }
-  */
-
-  /*
-  saveSpecification(exerciseDTO: ExerciseDTO) {
-    let specificationDTO = new SpecificationDTO();
-    specificationDTO.exercise = exerciseDTO;
-    specificationDTO = this.addSpecificationInfo(specificationDTO);
-    this.specificationService.save(specificationDTO).subscribe(data => {
-      specificationDTO = data;
-      this.addSpecificationToRoutine(specificationDTO);
-      });
-  }
-  */
-
-  /*
- addSpecification() {
-  if (this.name != '') {
-    this.newExercise();
-  } else if (this.selectedExercise != null) {
-    this.reUseExercise();
-  } 
-}
-  addSpecificationInfo(specificationDTO: SpecificationDTO) {
-    specificationDTO.repetitions = this.repetitions;
-    specificationDTO.secBreak = this.secBreak;
-    specificationDTO.other = this.other;
-    return specificationDTO;
-  }
-  */
-
-  /*
-  addSpecificationToRoutine(dto: SpecificationDTO) {
-    this.specifications = this.routineService.currentRoutine.specifications;
-    if (this.specifications == null) {
-      this.specifications = new Array();
-    }
-    this.specifications.push(dto);
-    console.log(this.specifications);
-    this.routineService.currentRoutine.specifications = this.specifications;
-  }
-  */
-
- addSpecification() {
-  let dto: SpecificationDTO = new SpecificationDTO();
-  dto.repetitions = this.repetitions;
-  dto.secBreak = this.secBreak;
-  dto.other = this.other;
-  let id = this.routineService.selectedRoutine.id;
-  this.routineService.addSpecification(id, dto).subscribe(data =>{
-    this.changed.emit();
-    this.changeStatus();
-  });
-}
 
 }
